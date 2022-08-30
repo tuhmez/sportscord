b4890d16b2cab1cbd681e5500b2a20f881f5f505
Chris Tamez <ctamez232@gmail.com>
2022-08-30 08:44:12 -0500

    fixing logs, dockerfile

--- dockerfile
+++ dockerfile
@@ -1,4 +1,4 @@
-FROM python:3.8.8
+FROM python:3.8.8-slim
 
 COPY . .
 RUN pip install -U -r requirements.txt
--- utils/logger.py
+++ utils/logger.py
@@ -1,6 +1,6 @@
 from utils.print import pretty_print
 
 def log(msg: str, is_ok: bool):
-  okay_header = '[OK]'
-  if is_ok is False: okay_header = '[ERR]'
+  okay_header = 'OK'
+  if is_ok is False: okay_header = 'ERR'
   pretty_print(f'{msg}', okay_header)
